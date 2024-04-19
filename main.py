1e67027312d6a4e2754a57d3ae203abdb71c3acd
Axel Carrillo <1722110634@utectulancingo.edu.mx>
2024-04-19 00:30:21 +0000

    UPDATE automaticamente

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ MYSQL_CONFIG = {
     'database': 'bmsrtwle6xkliwejunsm'
 }
 
-# Variable global para almacenar el valor de la tarjeta
-card_value = 0.0
+# Conectar a la base de datos
+conn = mysql.connector.connect(**MYSQL_CONFIG)
+cursor = conn.cursor(dictionary=True)
 
 # Modelo de datos para el valor de la tarjeta en el POST
 class CardValueInput(BaseModel):
@@ -32,21 +33,30 @@ app.add_middleware(
 # Endpoint para actualizar el valor de la tarjeta
 @app.post("/update-card-value/")
 async def update_card_value(card_input: CardValueInput):
+    # Actualizar el valor de la tarjeta global
     global card_value
     card_value = card_input.value
+    
+    # Aquí puedes realizar cualquier otra lógica de actualización necesaria en la base de datos
+    # Por ejemplo, podrías actualizar la base de datos con el nuevo valor de la tarjeta
+    
+    # Luego, devolver un mensaje de confirmación
     return {"message": "Card value updated successfully"}
 
 # Endpoint para leer el valor de la tarjeta y obtener datos de la base de datos según el valor RFID
 @app.get("/read-card-value/")
 async def read_card_value():
-    global card_value
-    conn = mysql.connector.connect(**MYSQL_CONFIG)
-    cursor = conn.cursor(dictionary=True)
+    # Obtener el valor de la tarjeta RFID
+    card_value = get_latest_card_value()  # Suponiendo que tienes una función para obtener el último valor de la tarjeta
+    
+    # Consultar la base de datos para obtener los datos del equipo asociados con el valor de la tarjeta
     query = "SELECT * FROM equipo WHERE rfid = %s"
     cursor.execute(query, (card_value,))
     equipo = cursor.fetchone()
-    cursor.close()  # Cierra el cursor después de leer los resultados de la consulta
-    conn.close()  # Cierra la conexión después de usarla
+    
+    # Manejar el caso donde no se encuentra ningún equipo con la tarjeta RFID proporcionada
     if not equipo:
         raise HTTPException(status_code=404, detail="Equipo no encontrado")
+    
+    # Devolver los datos del equipo
     return equipo
